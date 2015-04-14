d5e75b434bb791aa3df5c7224fe75c3240b549ad
Administrator <admin@local.host>
2015-04-14 11:08:25 +0200

    Home initial commit

---
+++ app/scripts/controllers/home.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name leagueOfApp.controller:HomeCtrl
+ * @description
+ * # HomeCtrl
+ * Sets up the svg map 
+ */
+
+ angular.module('leagueOfApp').controller('HomeCtrl', function ($scope) {
+ 	$scope.hello = 'hello';
+});
