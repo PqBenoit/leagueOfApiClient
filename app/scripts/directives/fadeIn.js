794385e13e84c67cc15e96187e74ad29605fe037
labalou <audeon.louis@gmail.com>
2015-04-17 18:50:41 +0200

    directive to animate with fade in effect

--- app/scripts/directives/fadeIn.js
+++ app/scripts/directives/fadeIn.js
@@ -1,19 +1,27 @@
 'use strict';
 
+// Copied from the Angular's sources.
+var NG_HIDE_CLASS = 'ng-hide';
+var NG_HIDE_IN_PROGRESS_CLASS = 'ng-hide-animate';
+
 /**
   * @class root.directives.fadeIn
   * @description To animate html elements with fade in style
   */
  angular
  	.module('leagueOfApp')
- 		.directive('fadeIn', function($timeout){
+ 		.directive('fadeIn', function($animate, $timeout){
 		    return {
 		        restrict: 'A',
-		        link: function($scope, $element, attrs){
-		            $element.addClass("ng-hide-remove");
-		            $element.on('load', function() {
-		                $element.addClass("ng-hide-add");
+		        link: function(scope, element, attrs){
+		            element.addClass("ng-hide");
+		            element.on('load', function() {
+		                $timeout(function () {
+		                    $animate.removeClass(element, NG_HIDE_CLASS, {
+		                        tempClasses: NG_HIDE_IN_PROGRESS_CLASS
+		                    });
+		                });
 		            });
 		        }
-		    }
+    		}
 });
